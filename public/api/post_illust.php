435066760afb0a9956c8547f3c2c5f22a8134659
45deg <45deg@example.com>
2015-09-05 16:20:28 +0900

    大幅アップデート

--- public/api/post_illust.php
+++ public/api/post_illust.php
@@ -12,6 +12,7 @@
        !isset($_POST['user_id']) || 
        !isset($_FILES['file'])) { // 入力がない
         http_response_code(500);
+        echo json_encode(array('error' => 'Invalid parameter'));
         die();
     }
 
@@ -23,31 +24,59 @@
 
     $db = new SQLite3('../database.db');
 
+    // title の作者を得る -> 無かったらエラー
+    $target_id = $db->querySingle("SELECT user_id FROM title WHERE user_id = '$user_id'");
+    if(!$target_id) {
+        http_response_code(500);
+        echo json_encode(array('error' => 'No such title'));
+        exit();
+    }
+
     $stmt = $db->prepare('INSERT INTO illust (title_id, user_id, likes, date) VALUES (:title_id, :user_id, 0, :date)');
     $stmt->bindValue(':title_id', $title_id, SQLITE3_INTEGER);
     $stmt->bindValue(':user_id', $user_id, SQLITE3_INTEGER);
     $stmt->bindValue(':date', $date, SQLITE3_TEXT);
 
     $result = $stmt->execute();
-    
+
     if(!$result) {
-        echo 'insert error';
         http_response_code(500);
-    } else {
-        $illust_id = $db->lastInsertRowID();
-        $illust_url = "http://{$_SERVER['HTTP_HOST']}/img/$illust_id";
-
-        if(move_uploaded_file($filedata['tmp_name'], $uploaddir . $illust_id)) {
-            echo json_encode(array(
-                'illust_id' => $illust_id,
-                'illust_url' => $illust_url,
-                'date' => $date
-            ));
-        } else {
-            echo 'missing data';
-            http_response_code(500);
-        }
-
+        echo json_encode(array('error' => 'Cannot insert illust'));
+        exit();
     }
 
+    $illust_id = $db->lastInsertRowID();
+    $illust_url = "http://{$_SERVER['HTTP_HOST']}/img/$illust_id";
+
+    // upload file
+
+    if(!move_uploaded_file($filedata['tmp_name'], $uploaddir . $illust_id)) {
+        http_response_code(500);
+        echo json_encode(array('error' => 'Cannot save illust'));
+        exit();
+    }
+
+    // update activity table
+    $stmt = $db->prepare('INSERT INTO activity (date, illust_id, title_id, target_user_id)'
+                        .' VALUES (:date, :illust_id, :title_id, :target_user_id)');
+
+    $stmt->bindValue(':title_id', $illust_id, SQLITE3_INTEGER);
+    $stmt->bindValue(':illust_id', $title_id, SQLITE3_INTEGER);
+    $stmt->bindValue(':target_user_id', $target_id, SQLITE3_INTEGER);
+    $stmt->bindValue(':date', $date, SQLITE3_TEXT);
+
+    $result = $stmt->execute();
+
+    if(!$result) {
+        http_response_code(500);
+        echo json_encode(array('error' => 'Cannot insert activity'));
+        exit();
+    }
+
+    // responce
+    echo json_encode(array(
+        'illust_id' => $illust_id,
+        'illust_url' => $illust_url,
+        'date' => $date
+    ));
 ?>
