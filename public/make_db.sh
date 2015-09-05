c3596cf967c7df6fa97335508607c31d100d8d53
45deg <45deg@example.com>
2015-09-05 16:13:02 +0900

    Add permission

--- public/make_db.sh
+++ public/make_db.sh
@@ -2,8 +2,9 @@
 
 echo "CREATE TABLE title(title_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, date TEXT, title TEXT);" > dbscheme
 echo "CREATE TABLE illust( illust_id INTEGER PRIMARY KEY AUTOINCREMENT, title_id INTEGER, user_id INTEGER, likes INTEGER, date TEXT);" >> dbscheme
-echo "CREATE TABLE activity( _id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, illust_id INTEGER, title_id INTEGERm target_user_id INTEGER);" >> dbscheme
+echo "CREATE TABLE activity( _id INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT, illust_id INTEGER, title_id INTEGER, target_user_id INTEGER);" >> dbscheme
 
 rm -f database.db
 cat dbscheme | sqlite3 database.db
+chmod 777 database.db
 rm -f dbscheme
