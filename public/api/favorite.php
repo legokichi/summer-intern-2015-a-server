d576cfaab428fbea60c079b4bae2890ca78ceeb1
45deg <45deg@example.com>
2015-09-05 17:00:13 +0900

    Implemented favorite

--- public/api/favorite.php
+++ public/api/favorite.php
@@ -9,6 +9,45 @@
 
     $date = time();
 
+    $db = new SQLite3('../database.db');
+
     // イラストテーブルを更新
+    $stmt = $db->prepare('UPDATE illust SET likes = likes + 1 WHERE illust_id = :illust_id');
+    $stmt->bindValue(':illust_id', $illust_id, SQLITE3_INTEGER);
+    $result = $stmt->execute();
+    if(!$result) {
+        http_response_code(500);
+        echo json_encode(array('error' => 'cannot update number'));
+        exit();
+    }
+
     // アクティビティテーブルを更新
+
+    // イラストテーブルからタイトルIDを取得
+    $title = $db->querySingle("SELECT title_id, user_id FROM illust WHERE illust_id = $illust_id");
+    if(!$title) {
+        http_response_code(500);
+        echo json_encode(array('error' => 'cannot get title_id'));
+        exit();
+    }
+
+    $title_id = $title['title_id'];
+    $target_id = $title['user_id'];
+
+    $stmt = $db->prepare('INSERT INTO activity (date, illust_id, title_id, target_user_id)'
+                        .' VALUES (:date, :illust_id, :title_id, :target_user_id)');
+    $stmt->bindValue(':date', $date, SQLITE3_TEXT);
+    $stmt->bindValue(':illust_id', $title_id, SQLITE3_INTEGER);
+    $stmt->bindValue(':title_id', $illust_id, SQLITE3_INTEGER);
+    $stmt->bindValue(':target_user_id', $target_id, SQLITE3_INTEGER);
+
+    $result = $stmt->execute();
+
+    if(!$result) {
+        http_response_code(500);
+        echo json_encode(array('error' => 'cannot insert activity'));
+        exit();
+    } 
+
+    echo json_encode(array('date' => $date));
 ?>
