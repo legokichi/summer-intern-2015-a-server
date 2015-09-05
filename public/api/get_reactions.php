873b28c1c4ef49767f6374365a08efc52645758d
45deg <45deg@example.com>
2015-09-05 17:26:58 +0900

    Now user_id is required

--- public/api/get_reactions.php
+++ public/api/get_reactions.php
@@ -1,9 +1,21 @@
 <?php
     $db = new SQLite3('../database.db');
 
-    $results = $db->query('SELECT type, activity.date, activity.title_id, title, illust_id' .
-                          ' FROM activity LEFT JOIN title ON title.title_id = activity.title_id' .
-                          ' ORDER BY _id DESC LIMIT 100');
+    if(!isset($_GET['user_id'])) {
+        http_response_code(500);
+        echo json_encode(array('error' => 'missing parameter'));
+        exit();
+    }
+
+    $user_id = $_GET['user_id'];
+
+    $stmt = $db->prepare('SELECT type, activity.date, activity.title_id, title, illust_id' .
+                         ' FROM activity LEFT JOIN title ON title.title_id = activity.title_id' .
+                         ' WHERE target_user_id=:user_id' .
+                         ' ORDER BY _id DESC LIMIT 100');
+    $stmt->bindValue('user_id', $user_id, SQLITE3_INTEGER);
+
+    $results = $stmt->execute();
     
     if(!$results) {
         http_response_code(500);
