a8f18aef11980a641f2a535e0de0393a8476bb4b
45deg <45deg@example.com>
2015-09-05 16:42:05 +0900

    指標を追加

--- public/api/favorite.php
+++ public/api/favorite.php
@@ -1,3 +1,14 @@
-{
-    "date": 1441346967 // UNIX TimeStamp
-}
+<?php
+    if(!isset($_POST['illust_id'])) {
+        http_response_code(500);
+        echo json_encode(array('error' => 'missing parameter'));
+        exit();
+    }
+
+    $illust_id = $_POST['illust_id'];
+
+    $date = time();
+
+    // イラストテーブルを更新
+    // アクティビティテーブルを更新
+?>
