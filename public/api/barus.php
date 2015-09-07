d43078e01f514ba528814d0e3fc9a9b3191acb93
45deg <45deg@example.com>
2015-09-07 16:51:01 +0900

    Use system()

--- public/api/barus.php
+++ public/api/barus.php
@@ -5,37 +5,23 @@
     if(!isset($_POST['barus'])) exit();
 
     $queries = array(
-        "DROP TABLE IF EXISTS title",
-        "DROP TABLE IF EXISTS illust",
-        "DROP TABLE IF EXISTS activity",
-        "DROP TABLE IF EXISTS user",
-        "CREATE TABLE title (title_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, user_name TEXT, date TEXT, title TEXT)",
-        "CREATE TABLE illust( illust_id INTEGER PRIMARY KEY AUTOINCREMENT, title_id INTEGER, user_id INTEGER, user_name TEXT, likes INTEGER, date TEXT)",
-        "CREATE TABLE activity( _id INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, date TEXT, illust_id INTEGER, title_id INTEGER, target_user_id INTEGER)",
-        "CREATE TABLE user ( user_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)"
+        "DROP TABLE IF EXISTS title;",
+        "DROP TABLE IF EXISTS illust;",
+        "DROP TABLE IF EXISTS activity;",
+        "DROP TABLE IF EXISTS user;",
+        "CREATE TABLE title (title_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, user_name TEXT, date TEXT, title TEXT);",
+        "CREATE TABLE illust( illust_id INTEGER PRIMARY KEY AUTOINCREMENT, title_id INTEGER, user_id INTEGER, user_name TEXT, likes INTEGER, date TEXT);",
+        "CREATE TABLE activity( _id INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, date TEXT, illust_id INTEGER, title_id INTEGER, target_user_id INTEGER);",
+        "CREATE TABLE user ( user_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);"
     );
 
-    echo implode(';', $queries);
-
     try {
-        $db = new SQLite3('../database.db');
-        $db->exec("BEGIN DEFERRED;");
-
-        foreach ($query as $queries) {
-            if(!$db->exec($query)) {
-                $db->exec("ROLLBACK;");
-                print 'ERROR;';
-                exit();
-            }
-        }
-
+        system('echo "' . implode('', $queries) . '" | sqlite3 ../database.db');
     } catch (Exception $e) {
         $db->exec("ROLLBACK;");
         print 'ERROR!';
         exit();
     }
-
-    $db->exec("COMMIT;");
 
     // remove all images
     array_map('unlink', glob("../img/*"));
