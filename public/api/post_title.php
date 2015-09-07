13c1a5966986b05f397d1cade42eb7dd9a30316e
45deg <45deg@example.com>
2015-09-07 16:27:13 +0900

    Added username response to post_title

--- public/api/post_title.php
+++ public/api/post_title.php
@@ -8,21 +8,24 @@
             "count": 0,
         }
     */
-    if(!isset($_POST['title']) || !isset($_POST['user_id'])) { // 入力がない
+    if(!isset($_POST['title']) || !isset($_POST['user_id']) 
+       || !isset($_POST['user_name'])) { // 入力がない
         http_response_code(500);
         die();
     }
 
     $title = $_POST['title'];
     $user_id = $_POST['user_id'];
+    $user_name = $_POST['user_name'];
 
     $date = time();
 
     $db = new SQLite3('../database.db');
 
-    $stmt = $db->prepare('INSERT INTO title (user_id, date, title) VALUES (:user_id ,:date, :title)');
+    $stmt = $db->prepare('INSERT INTO title (user_id, user_name, date, title) VALUES (:user_id, :user_name, :date, :title)');
     $stmt->bindValue(':user_id', $user_id, SQLITE3_INTEGER);
     $stmt->bindValue(':date', $date, SQLITE3_TEXT);
+    $stmt->bindValue(':user_name', $user_name, SQLITE3_TEXT);
     $stmt->bindValue(':title', $title, SQLITE3_TEXT);
 
     $result = $stmt->execute();
@@ -36,6 +39,7 @@
         echo json_encode(array(
             'title_id' => $db->lastInsertRowId(),
             'date' => $date,
+            'user_name' => $user_name,
             'title' => $title,
             'count' => 0
         ));
