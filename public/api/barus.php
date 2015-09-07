4a81279b86bb65349f3ecf50385888cfa6494731
45deg <45deg@example.com>
2015-09-07 16:46:02 +0900

    Refined DBinit

--- public/api/barus.php
+++ public/api/barus.php
@@ -2,26 +2,23 @@
 <input type="submit" name="barus" value="バルス">
 </form>
 <?php
-
     if(!isset($_POST['barus'])) exit();
 
-    $db = new SQLite3('../database.db');
-
-    $db->exec("BEGIN DEFERRED;");
-
     $queries = array(
-        "DROP TABLE title",
-        "DROP TABLE illust",
-        "DROP TABLE activity",
-        "DROP TABLE user",
+        "DROP TABLE IF EXISTS title",
+        "DROP TABLE IF EXISTS illust",
+        "DROP TABLE IF EXISTS activity",
+        "DROP TABLE IF EXISTS user",
         "CREATE TABLE title (title_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, user_name TEXT, date TEXT, title TEXT)",
         "CREATE TABLE illust( illust_id INTEGER PRIMARY KEY AUTOINCREMENT, title_id INTEGER, user_id INTEGER, user_name TEXT, likes INTEGER, date TEXT)",
         "CREATE TABLE activity( _id INTEGER PRIMARY KEY AUTOINCREMENT, type INTEGER, date TEXT, illust_id INTEGER, title_id INTEGER, target_user_id INTEGER)",
         "CREATE TABLE user ( user_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)"
     );
 
-    try {
+    echo implode(';', $queries);
 
+    try {
+        $db = new SQLite3('../database.db');
         $db->exec("BEGIN DEFERRED;");
 
         foreach ($query as $queries) {
@@ -39,6 +36,9 @@
     }
 
     $db->exec("COMMIT;");
+
+    // remove all images
+    array_map('unlink', glob("../img/*"));
 
     echo ('<img src="http://middle-edge.jp/file/parts/I0000604/abf86a576cf524c2167c5e8614fd271e.jpg">');
 ?>
