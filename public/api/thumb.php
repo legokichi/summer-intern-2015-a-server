1bf2393beaa29a7285733f93f874ff177e9bb99f
45deg <45deg@example.com>
2015-09-10 00:10:59 +0900

    Using cache

--- public/api/thumb.php
+++ public/api/thumb.php
@@ -3,31 +3,34 @@ $width = 200;
 $id = $_GET["illust_id"];
 $url = "../img/$id";
 
-list($image_w, $image_h) = getimagesize($url);
+// no cache -> create thumbnail
+if(!file_exists($url)){
+    list($image_w, $image_h) = getimagesize($url);
 
-$proportion = $image_w / $image_h;
-$height = $width / $proportion;
-if($proportion < 1){
-    $height = $width;
-    $width = $width * $proportion;
+    $proportion = $image_w / $image_h;
+    $height = $width / $proportion;
+    if($proportion < 1){
+        $height = $width;
+        $width = $width * $proportion;
+    }
+
+    $canvas = imagecreatetruecolor($width, $height);
+
+    $image = imagecreatefromjpeg($url);
+
+    imagecopyresampled($canvas,
+        $image,
+        0, 0, 0, 0,
+        $width, $height, $image_w, $image_h
+    );
+
+    // 画像を出力する
+    imagejpeg($canvas,
+        "../img/thumb/$id",
+        50
+    );
+
+    imagedestroy($canvas);
 }
-
-$canvas = imagecreatetruecolor($width, $height);
-
-$image = imagecreatefromjpeg($url);
-
-imagecopyresampled($canvas,
-    $image,
-    0, 0, 0, 0,
-    $width, $height, $image_w, $image_h
-);
-
-// 画像を出力する
-imagejpeg($canvas,
-    "../img/thumb/$id",
-    50
-);
-
-imagedestroy($canvas);
 
 header("Location: /img/thumb/$id");
