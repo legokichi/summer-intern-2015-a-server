2785ffd3c81fbef9d248f91a130afd9a5650b298
45deg <45deg@example.com>
2015-09-10 00:08:25 +0900

    Implemented thumbnail

---
+++ public/api/thumb.php
@@ -0,0 +1,33 @@
+<?php
+$width = 200;
+$id = $_GET["illust_id"];
+$url = "../img/$id";
+
+list($image_w, $image_h) = getimagesize($url);
+
+$proportion = $image_w / $image_h;
+$height = $width / $proportion;
+if($proportion < 1){
+    $height = $width;
+    $width = $width * $proportion;
+}
+
+$canvas = imagecreatetruecolor($width, $height);
+
+$image = imagecreatefromjpeg($url);
+
+imagecopyresampled($canvas,
+    $image,
+    0, 0, 0, 0,
+    $width, $height, $image_w, $image_h
+);
+
+// 画像を出力する
+imagejpeg($canvas,
+    "../img/thumb/$id",
+    50
+);
+
+imagedestroy($canvas);
+
+header("Location: /img/thumb/$id");
